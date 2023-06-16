c0a447e56cea7f1c121d0867f21b5326e7a4643c
redr3M <136112257+redr3M@users.noreply.github.com>
2023-06-16 22:59:15 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+close_options = ['Да', 'Нет']
+questions = [
+    {'text': 'Играешь ли ты в компьютерные игры?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Играешь ли в онлайн игры? ', 'qtype': 'closed',
+     'options': close_options},
+     {'text': 'Считаете ли вы  Dota 2 хорошей игрой', 'qtype': 'closed',
+     'options': close_options},
+    {'text': 'Разбивали ли вы что-нибудь во время игры?', 'qtype': 'closed',
+     'options': close_options},
+    {'text': 'Вы играете больше 5 часов в день?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Пойдете ли вы на все, чтобы получить то, что вам очень хочется?', 'qtype': 'closed',
+     'options': close_options},
+    {'text': 'Бросаете игры на время сессии?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Есть ли у вас допса в этом семестре?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Уверены ли вы, что когда-нибудь станете знаменитостью?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Умеете ли вы вовремя остановиться, если чувствуете, что начинаете проигрывать?', 'qtype': 'closed',
+     'options': close_options},
+    {'text': 'Курение убивает?', 'qtype': 'closed', 'options': close_options},
+    {'text': 'Понравился опрос?', 'qtype': 'closed', 'options': close_options}
+]
